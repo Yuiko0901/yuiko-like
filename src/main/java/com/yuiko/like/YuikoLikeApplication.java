f9bda29c26f38a0625c8c5316512fdeb1fe4d7ce
wuwentao <fq85163295@163.com>
2025-04-30 14:50:28 +0800

    1.init 项目初始化

---
+++ src/main/java/com/yuiko/like/YuikoLikeApplication.java
@@ -0,0 +1,13 @@
+package com.yuiko.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuikoLikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuikoLikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuiko/like/YuikoLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuiko.like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuikoLikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
